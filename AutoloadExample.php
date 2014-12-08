595b4320821901b7e4eadeccc3966880ec03d6ef
marina <marina_bakum@mail.ru>
2014-12-08 12:17:29 +0200

    added main project files

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace marina\simple;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
